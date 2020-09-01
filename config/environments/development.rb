fdbd4108e58d987578df5174101fc1f204e3d50f
Ubuntu <ubuntu@ip-172-31-85-52.ec2.internal>
2020-09-01 13:16:24 +0000

    klmdls

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,8 @@ Rails.application.configure do
   
   config.hosts << "90ef755cecdd4da7bb8c12e5658b5561.vfs.cloud9.us-east-1.amazonaws.com"
   
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://90ef755cecdd4da7bb8c12e5658b5561.vfs.cloud9.us-east-1.amazonaws.com' }
+  config.action_mailer.delivery_method = :test
 
   # Settings specified here will take precedence over those in config/application.rb.
 
